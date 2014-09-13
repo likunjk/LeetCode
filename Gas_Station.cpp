995a8b1e36ae79c8d07a1ed9d557235c8a3fdd1c
Kun Lee <likunjk@gmail.com>
2014-09-13 22:16:17 +0800

    Create Gas_Station.cpp

---
+++ Gas_Station.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    // 其实就是一个环路，将加气站头尾连起来，只要能够连续正常经过n个加气站，那么就说明我们可以完成环游
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) 
+    {
+        int n = gas.size();
+        
+        int count = 0;
+        int total = 0;
+        for(int i=0; i<2*n; ++i)
+        {
+            int index = i%n;
+            total += gas[index];
+            
+            if(total >= cost[index])
+            {
+                count++;
+                total -= cost[index];
+                if(count==n)
+                {
+                    //因为题目保证了结果的唯一性，因此可以直接返回
+                    return i-n+1; 
+                }
+            }
+            else    //说明从该点不能到达下一点，那么直接从下一点开始
+            {
+                count = 0;
+                total = 0;
+            }
+        }
+        
+        return -1;  //不能完成环游
+    }
+};
