8df6c0d5aa90051e1a344be81d7c0e1d00611fc9
Kun Lee <likunjk@gmail.com>
2014-09-18 23:59:29 +0800

    Update Search_in_Rotated_Sorted_Array.cpp

--- Search_in_Rotated_Sorted_Array.cpp
+++ Search_in_Rotated_Sorted_Array.cpp
@@ -10,9 +10,9 @@ public:
             int mid = (left+right)/2;
             if(A[mid]==target)  //找到啦
                 return mid;
-            else if(A[mid] >= A[0])  //mid位于前半段, 注意取到等号
+            else if(A[mid] >= A[left])  //mid位于前半段, 注意取到等号
             {
-                if(target < A[mid] && target >= A[0])   //只有这种情况，target才位于左边
+                if(target < A[mid] && target >= A[left])   //只有这种情况，target才位于左边
                     right = mid-1;
                 else
                     left = mid+1;
@@ -20,7 +20,7 @@ public:
             }
             else
             {
-                if(target > A[mid] && target <= A[n-1]) //只有这种情况，target才位于右边
+                if(target > A[mid] && target <= A[right]) //只有这种情况，target才位于右边
                     left = mid+1;
                 else
                     right = mid-1;
